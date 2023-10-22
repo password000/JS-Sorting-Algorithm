e3284a5f26c28dc8b6b4bb6b47ebeac310e4e97b
password000 <148674602+password000@users.noreply.github.com>
2023-10-22 14:39:28 +0800

    Create MergeSort.cpp

---
+++ MergeSort.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+using namespace std;
+
+// Merge two sorted subarrays into one sorted array
+void merge(int arr[], int left, int mid, int right) {
+    int i, j, k;
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    // Create temporary arrays
+    int L[n1], R[n2];
+
+    // Copy data to temporary arrays
+    for (i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+
+    // Merge the temporary arrays back into arr[left..right]
+    i = 0; // Initial index of first subarray
+    j = 0; // Initial index of second subarray
+    k = left; // Initial index of merged subarray
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    // Copy the remaining elements of L[], if there are any
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    // Copy the remaining elements of R[], if there are any
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+// Merge sort function
+void mergeSort(int arr[], int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+
+        // Sort first and second halves
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        // Merge the sorted halves
+        merge(arr, left, mid, right);
+    }
+}
+
+// Function to print an array
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+// Driver code
+int main() {
+    const int size = 10000;
+    int arr[size];
+
+    // Generate random numbers for the array
+    for (int i = 0; i < size; i++)
+        arr[i] = rand() % 10000;
+
+    cout << "Original array: ";
+    // printArray(arr, size); // Uncomment this line to print the original array
+
+    mergeSort(arr, 0, size - 1);
+
+    cout << "Sorted array: ";
+    // printArray(arr, size); // Uncomment this line to print the sorted array
+
+    return 0;
+}
